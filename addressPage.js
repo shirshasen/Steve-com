204d316850d14ade6c5c3bcb9044e4e1e54e2a04
Humna <humnasultan@gmail.com>
2025-03-19 00:52:25 -0400

    popup form

--- addressPage.js
+++ addressPage.js
@@ -5,3 +5,17 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+
+/*
+function displayValue() {
+            // Get the value from the dropdown
+            const selectedValue = document.getElementById("location").value;
+
+            // Print the value on the screen
+            if (selectedValue) {
+                document.getElementById("output").innerText = `You selected: ${selectedValue}`;
+            } else {
+                document.getElementById("output").innerText = "Please select a country/region.";
+            }
+        }
+*/

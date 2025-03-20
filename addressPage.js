59d7300daf66d027c053082083935f3a65038840
Humna <humnasultan@gmail.com>
2025-03-20 18:36:51 -0400

    currently works

--- addressPage.js
+++ addressPage.js
@@ -6,6 +6,24 @@ function openForm() {
     document.getElementById("myForm").style.display = "none";
   }
 
+  function updateAddress() {
+
+    document.getElementById("myForm").style.display = "none";
+
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var country = document.getElementById("region").value;
+    var address1 = document.getElementById("address1").value;
+    var address2 = document.getElementById("address2").value;
+    var city = document.getElementById("city").value;
+    var zipcode = document.getElementById("zip").value;
+
+    var formattedAddress = name + "<br>" + phone + "<br><br>" + country + "<br>" + address1 + address2 + "<br>" + city + "<br>" + zipcode;
+
+    console.log(formattedAddress);
+    document.getElementById("addressVal").innerHTML = formattedAddress;
+
+  }
 /*
 function displayValue() {
             // Get the value from the dropdown
